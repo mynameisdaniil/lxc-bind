f4d05562b572b703948c3be37ce96b5049a0a874
Daniil Sobol <sobol.daniil@gmail.com>
2014-07-27 03:30:41 +0400

    GPLing the code

--- src/main.c
+++ src/main.c
@@ -1,3 +1,22 @@
+/*
+  Copyright © 2014 Daniil Sobol
+
+  This file is part of lxc-bind.
+
+  lxc-bind is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  lxc-bind is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with lxc-bind. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "main.h"
 #include "version.h"
 
